abe3de843504a6a60f826157077ced0312768438
Jaspreet <jaspreetmehra185@gmail.com>
2024-07-28 22:05:10 +0530

    create a project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@nextui-org/react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js with NextUI and TypeScript</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div className="container w-100 h-100 bg-red-300">
+      <div >hello</div>
+    </div>
+  )
+}
